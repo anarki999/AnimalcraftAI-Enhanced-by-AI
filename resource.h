7ce5d144cf30eca4b711552870dda9b7367f6883
anarki999 <cyrus.wongcp@gmail.com>
2026-07-28 01:48:08 +0800

    Add Ponder dialog resource definitions

--- resource.h
+++ resource.h
@@ -1,52 +1,55 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-#define IDI_APPICON    100
+#define IDI_APPICON 100
 
-#define IDB_BOARD      202
-#define IDB_TRAP       203
-#define IDB_DEN        204
-#define IDB_SELECTED   201
-#define IDB_RE         208
-#define IDB_RL         209
-#define IDB_RT         210
-#define IDB_RP         211
-#define IDB_RW         212
-#define IDB_RD         213
-#define IDB_RC         214
-#define IDB_RM         215
-#define IDB_BE         216
-#define IDB_BL         217
-#define IDB_BT         218
-#define IDB_BP         219
-#define IDB_BW         220
-#define IDB_BD         221
-#define IDB_BC         222
-#define IDB_BM         223
+#define IDB_BOARD 202
+#define IDB_TRAP 203
+#define IDB_DEN 204
+#define IDB_SELECTED 201
+#define IDB_RE 208
+#define IDB_RL 209
+#define IDB_RT 210
+#define IDB_RP 211
+#define IDB_RW 212
+#define IDB_RD 213
+#define IDB_RC 214
+#define IDB_RM 215
+#define IDB_BE 216
+#define IDB_BL 217
+#define IDB_BT 218
+#define IDB_BP 219
+#define IDB_BW 220
+#define IDB_BD 221
+#define IDB_BC 222
+#define IDB_BM 223
 
-#define IDR_START      300
-#define IDR_MOUSE      308
-#define IDR_CAT        307
-#define IDR_DOG        306
-#define IDR_WOLF       305
-#define IDR_LEOPARD    304
-#define IDR_TIGER      303
-#define IDR_LION       302
-#define IDR_ELEPHANT   301
+#define IDR_START 300
+#define IDR_MOUSE 308
+#define IDR_CAT 307
+#define IDR_DOG 306
+#define IDR_WOLF 305
+#define IDR_LEOPARD 304
+#define IDR_TIGER 303
+#define IDR_LION 302
+#define IDR_ELEPHANT 301
 
 #define IDC_EDIT1 1032 
 
-#define IDM_MAINMENU   400
-#define IDM_FILE_RED   401
+#define IDM_MAINMENU 400
+#define IDM_FILE_RED 401
 #define IDM_FILE_BLACK 402
 #define IDM_FILE_TRAINING 403
 #define IDM_STANDARD 404
 #define IDM_HANDICAP 405
 #define IDM_LOADFEN 406
 #define IDM_UNDO 407
-#define IDM_FILE_EXIT  408
+#define IDM_FILE_EXIT 408
 #define IDM_OUTPUTFEN 409
 
 #define IDD_ANALYZE_DIALOG 410
+
+#define IDD_PONDER_DIALOG 500
+#define IDC_PONDER_EDIT   501
 
 #endif
